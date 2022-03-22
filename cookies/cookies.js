5716867be2e3a837692b5965244afcb231848b07
Pavel Morozuk <morozuki@mail.ru>
2022-03-23 00:19:07 +0300

    Cookies and vebcola.

---
+++ cookies/cookies.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    let button = document.getElementById("bake");
+    button.onclick = function() {
+        console.log("Time to bake the cookies.");
+        cookies.bake(2500);
+    };
+};
+let cookies = {
+    instructions: "Preheat oven to 350...",
+    bake: function(time) {
+        console.log("Baking the cookies.");
+        setTimeout(done, time);
+    }
+};
+
+function done() {
+    alert("Cookies are ready, take them out to cool.");
+    console.log("Cooling the cookies.");
+    setTimeout(function() {
+        alert("Cookies are cool, time to eat!");
+        }, 1000);
+}
